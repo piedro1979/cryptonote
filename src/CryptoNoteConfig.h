5962afd63f2221439a01b732a8b39ebe4e8e4b53
Pdro <piedro@gmx.at>
2017-06-25 06:06:03 +0200

    updated 2nd seednode

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -107,6 +107,7 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
   "spunk-coin.com:45110",
+  "pdromg.ddns.net:45110",
   //"your_seed_ip1.com:8080",
   //"your_seed_ip2.com:8080",
 };
